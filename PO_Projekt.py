7371bd5c6fb465b37f4e56c565e18dfde9a7be3c
kmiecikmichal <48928449+kmiecikmichal@users.noreply.github.com>
2019-05-13 16:49:29 +0200

    Add files via upload

--- PO_Projekt.py
+++ PO_Projekt.py
@@ -18,20 +18,44 @@ with open("PolishBase.txt", encoding="utf8") as pol:
 with open("EnglishBase.txt", encoding="utf8") as eng:
      words_eng = eng.read().split() 
 
+with open("FrenchBase.txt", encoding="utf8") as fra:
+     words_fra = fra.read().split()
+
+with open("GermanBase.txt", encoding="utf8") as ger:
+     words_ger = ger.read().split() 
+
+with open("ItalianBase.txt", encoding="utf8") as ita:
+     words_ita = ita.read().split()
+
+with open("SpanishBase.txt", encoding="utf8") as esp:
+     words_esp = esp.read().split() 
+
 inter_pol = set(words_pol) & set(words_txt)
 inter_eng = set(words_eng) & set(words_txt)
+inter_fra = set(words_fra) & set(words_txt)
+inter_ger = set(words_ger) & set(words_txt)
+inter_ita = set(words_ita) & set(words_txt)
+inter_esp = set(words_esp) & set(words_txt)
 #dodać coś że nazwa własna to to co wystąpi w kilku językach na raz
 #coś z set(inter_pol) & set(inter_eng)
 
-if   len(inter_pol) >= 3 and len(inter_pol) > len(inter_eng):
+if   len(inter_pol) > len(inter_eng) and len(inter_pol) > len(inter_fra) and len(inter_pol) > len(inter_ger) and len(inter_pol) > len(inter_ita) and len(inter_pol) > len(inter_esp):
      print("Język polski")
-elif 1 <= len(words_pol) < 3 and len(inter_pol) > len(inter_eng):
-     print("Prawdopodobnie język polski")
-elif len(inter_eng) >= 3 and len(inter_pol) < len(inter_eng):
+
+elif len(inter_eng) > len(inter_pol) and len(inter_eng) > len(inter_fra) and len(inter_eng) > len(inter_ger) and len(inter_eng) > len(inter_ita) and len(inter_eng) > len(inter_esp):
      print("Język angielski")
-elif 1 <= len(inter_eng) < 3 and len(inter_pol) < len(inter_eng):
-     print("Prawdopodobnie język angielski")
+
+elif len(inter_fra) > len(inter_pol) and len(inter_fra) > len(inter_eng) and len(inter_fra) > len(inter_ger) and len(inter_fra) > len(inter_ita) and len(inter_fra) > len(inter_esp):
+     print("Język francuski")
+
+elif len(inter_ger) > len(inter_pol) and len(inter_ger) > len(inter_eng) and len(inter_ger) > len(inter_fra) and len(inter_ger) > len(inter_ita) and len(inter_ger) > len(inter_esp):
+     print("Język niemiecki")
+
+elif len(inter_ita) > len(inter_pol) and len(inter_ita) > len(inter_eng) and len(inter_ita) > len(inter_fra) and len(inter_ita) > len(inter_ger) and len(inter_ita) > len(inter_esp):
+     print("Język włoski")
+
+elif len(inter_esp) > len(inter_pol) and len(inter_esp) > len(inter_eng) and len(inter_esp) > len(inter_fra) and len(inter_esp) > len(inter_ger) and len(inter_esp) > len(inter_ita):
+     print("Język hiszpański")
+
 else:
      print("Brak języka w bazie lub niewystarczająca ilość danych")
-
-#zmienić te ify elsy, coś w razie nazw własnych(brak nazw własnych w bazie?
--- PO_Projekt_Base_Modeling.py
+++ PO_Projekt_Base_Modeling.py
@@ -1,7 +1,8 @@
 from collections import Counter
 from collections import defaultdict
+import re
 
-with open("EnglishBase.txt", encoding="utf8") as f:
+with open("ItalianBase.txt", encoding="utf8") as f:
     words = f.read().split()
 
 word_dict = defaultdict(list)
@@ -12,10 +13,12 @@ for i in range(0,len(word_dict[word])):
     words[i] = "".join(words[i])
 
 UniqW = Counter(words)
-s = "\n".join(UniqW.keys())
+w = "\n".join(UniqW.keys()).lower() #unikalne znaki, zapis lowercase
+s = re.sub("\d+", "", w) #usuwa cyfry
+#s = re.sub("«", "", w) #do wpisywania innych znaków do usunięcia » «
 
 #write
-with open("EnglishBase.txt", "w+", encoding = "utf8") as w:
+with open("ItalianBase.txt", "w+", encoding = "utf8") as w:
     w.write(s)
 w.close()
 #append
